17dbfd6ed24e880eaaec5c1b78999d1f07b9c6b4
unknown <aavik743@gmail.com>
2022-05-10 23:19:26 +0530

    [Abhik]: added asyncio

--- multiple_city_async.py
+++ multiple_city_async.py
@@ -2,7 +2,7 @@ import codecs
 import csv
 import json
 import time
-
+import asyncio
 import requests
 
 city_list = ['mumbai',
@@ -29,7 +29,7 @@ city_list = ['mumbai',
              ]
 
 
-def get_data(city_name):
+async def get_data(city_name):
     base_url = 'https://api.openweathermap.org/data/2.5/weather?appid=0c42f7f6b53b244c78a418f4f181282a&q='
     url = base_url + city_name
     response = requests.get(url)  # get weather from file by city name
@@ -37,10 +37,10 @@ def get_data(city_name):
 
     text = response.text
     json_data = {"record": json.loads(text)}
-    update_data(json_data)
+    await update_data(json_data)
 
 
-def update_data(json_data):
+async def update_data(json_data):
     with codecs.open('data.csv', 'a', encoding='utf-8') as file:
         csv_file = csv.writer(file)
         for data in json_data.values():
@@ -48,14 +48,15 @@ def update_data(json_data):
                                data['main']['temp_max'], data['main']['temp_min']])
 
 
-def main(cities):
+async def main(cities):
     for city in cities:
-        get_data(city)
+        task = asyncio.create_task(get_data(city))
+        await task
 
 
 if __name__ == '__main__':
     start_time = time.perf_counter()
-    main(city_list)
+    asyncio.run(main(city_list))
     end_time = time.perf_counter()
     total_time = end_time - start_time
     print(total_time)
